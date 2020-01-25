3cad945d15d10177aa8b917140efb308023a12bd
jeremiah66 <50328513+jeremiah66@users.noreply.github.com>
2020-01-25 19:03:58 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -232,7 +232,7 @@ $('#service-lists').on('click','.donateBtn', async function(e){
     $('#loading').show();
     const service_id = e.target.id;
     console.log("serv_id= "+service_id)
-    const comment = $('input[id='+service_id+']').val();
+    const comment = $('input[id='+'c'+service_id+']').val();
     console.log("comm= "+comment)
    await contractCall('addReview',[service_id, comment], comment);
      renderService();
